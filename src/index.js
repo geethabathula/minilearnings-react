9ade2760adb039d526ff1090023be6f5bb790185
geethabathula <bgeethasupriya.99@gmail.com>
2024-04-29 21:51:40 +0530

    React BTS (diffing, state batching)

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Modal, { Success, Error, Warning } from './ComponentComposition/Modal';
+// import Modal, { Success, Error, Warning } from './ComponentComposition/Modal';
+import Reactbts from './ReactBTS/Reactbts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Modal children={<Success />} />
+    {/* <Modal children={<Success />} />
     <Modal children={<Warning />} />
-    <Modal children={<Error />} />
+    <Modal children={<Error />} /> */}
+    <Reactbts />
   </React.StrictMode>
 );
 
