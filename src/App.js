68ce121bdcccdbc8fac720cf9e72a42e1a69db3e
geethabathula <bgeethasupriya.99@gmail.com>
2024-05-02 22:37:41 +0530

    Helpeing Ammu on updating select value onChange

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useState } from 'react';
 import './index.css';
 
+//Helping Ammuu on updating a selectvalue
 function App() {
-  return <>
+  const [formData, setFormData] = useState({ selectValue: "", textValue: "hi" });
 
-  </>
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "selectValue") {
+      setFormData({ ...formData, [id]: value });
+    }
+    if (id === "textValue") {
+      setFormData({ ...formData, [id]: value });
+    }
+  };
+
+  return (
+    <>
+      <form>
+        <select
+          id="selectValue"
+          value={formData.selectValue}
+          onChange={handleChange}
+        >
+          <option value="">Select Option</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <input
+          id="textValue"
+          type="text"
+          value={formData.textValue}
+          onChange={handleChange}
+        />
+      </form>
+      <h1>{formData.selectValue} and {formData.textValue}</h1>
+    </>
+  );
 }
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Basic from './MovieDB/Basic';
-// import App from './App';
+import App from './App';
 // import Modal, { Success, Error, Warning } from './ComponentComposition/Modal';
 // import Reactbts from './ReactBTS/Reactbts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <Modal children={<Success />} />
     <Modal children={<Warning />} />
     <Modal children={<Error />} /> */}
     {/* <Reactbts /> */}
-    <Basic />
+    {/* <Basic /> */}
   </React.StrictMode>
 );
 
